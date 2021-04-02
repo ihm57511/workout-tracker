0f563ff008864049266ebd260efd0257d952ad10
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-02 15:47:07 -0400

    corrected models

--- models/exercise.js
+++ models/exercise.js
@@ -2,44 +2,50 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const ExerciseSchema = new Schema ({
-  name: {
-    type: String,
-    trim: false,
-    required: "Name of excorsixe is required."
-    },
-  
-    weight: {
-      type: Double,
-      trim: true,
-      required: "Weight must be specified."
-    },
-  
-    sets: {
-      type: Double,
-      trim: true,
-      required: "Number of sets must be sepcified."
-    },
-  
-    reps: {
-      type: Double,
-      trim: true,
-      required: "Number of reps must be specified."
-    },
+const ExerciseSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
 
-    distance: {
-      type: Double,
-      trim: true,
-      require: "Distance must be specified."
-    },
-  
-    duration: {
-      type: Double,
-      trim: true,
-      required: "Duration must be specified."
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        require: "Please specify type of exercise."
+      },
+    
+      name: {
+        type: String,
+        trim: true,
+        required: "Name of  is required."
+      },
+    
+      weight: {
+        type: Number,  
+      },
+    
+      sets: {
+        type: Number,
+      },
+    
+      reps: {
+        type: Number,
+      },
+    
+      distance: {
+        type: Number,
+      },
+    
+      duration: {
+        type: Number,
+        required: "Duration must be specified."
+      }
     }
+  ]
 });
 
 const Exercise = mongoose.model('Exercise', ExerciseSchema);
 
-module.exports = Exercise; 
+module.exports = Exercise;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,3 @@
 module.exports = {
-  Cardio: require('./cardio'),
-  Resistance: require('./resistance'),
-  Exercise: require('./exercise')
+  Workout: require('./exercise')
 };

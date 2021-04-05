b32793a340ea35fa9bde50ae8e2f4001d8973e4c
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-05 17:42:54 -0400

    adjusted mongodb options in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 const apiRoutes = require('./routes/apiRoutes');
 const homeRoutes = require('./routes/homeRoutes');
@@ -13,12 +13,19 @@ const app = express();
 
 app.use(logger('dev'));
 
-app.use(express.urlencoded({estended: true}));
+app.use(express.urlencoded({ estended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 app.use(apiRoutes);
 app.use(homeRoutes);
